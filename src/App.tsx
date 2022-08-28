b49e79f1b0b6ae0382cc98b4e8647acf826968e7
Javier Torales <82301704+JtoralesK@users.noreply.github.com>
2022-08-28 00:13:57 -0300

    cargando pagina

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-
+import { About } from "./components//about/about";
+import { Contact } from "./components//contact";
+import {Intro}from"./components//intro"
+import { ListProjects } from "./components//listProjects";
+import { Pg } from "./components//pantallasGrandes";
+import {Header} from"./components//header"
+import { Toggle } from "./components//toggle";
+import { Footer } from "./components/footer";
 function App() {
-  const [count, setCount] = useState(0)
+  const [mode , setMode] = useState(true)
 
+  const handle =()=>{                
+      setMode(!mode)
+    }
+  
   return (
-   <div>hola jajaj</div>
+      <div className={mode?"dark-mode":"normal-mode"}>
+      <Pg>
+       <Header>
+       <Toggle change={handle}/>
+       </Header>
+       <Intro/>
+       <ListProjects/>
+       <About/>
+       <Contact/>
+      </Pg>
+       <Footer/>
+
+  
+      </div>
   )
 }
 
